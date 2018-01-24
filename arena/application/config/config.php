d34354da4c08313a92267acd76d5fdfa12a8922f
Sohel <srana3170@gmail.com>
2018-01-24 12:21:26 +0600

    Change local configuration

--- arena/application/config/config.php
+++ arena/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/arena/';
+$config['base_url'] = 'http://www.oceanarenatravels.com//beta/';
 
 /*
 |--------------------------------------------------------------------------
