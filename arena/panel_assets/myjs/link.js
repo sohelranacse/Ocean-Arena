2b74d831ca6937afeb504178767ac15199db2820
Sohel <srana3170@gmail.com>
2018-01-24 13:27:52 +0600

    Link change

--- arena/panel_assets/myjs/link.js
+++ arena/panel_assets/myjs/link.js
@@ -1,5 +1,5 @@
 function links(){
 	//var li="http://desiree.thirdhandbd.com/";
-	var li="http://www.oceanarenatravels.com//beta/";
+	var li="http://www.oceanarenatravels.com/beta/";
 	return li;
 }
