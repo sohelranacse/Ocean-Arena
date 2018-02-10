ed4719b73e561b860d06c06c72ef8b877b48d872
Tanzir Rahman <tanzir71@gmail.com>
2018-02-10 15:43:41 +0600

    Updates.

--- arena/application/config/config.php
+++ arena/application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://www.oceanarenatravels.com/beta/';
-//$config['base_url'] = 'http://localhost/arena/';
 
 /*
 |--------------------------------------------------------------------------
